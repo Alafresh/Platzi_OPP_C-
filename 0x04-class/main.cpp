016ed0208830cbac028688de1fadef171af0397c
Alafresh <alafresh16@gmail.com>
2021-08-05 22:28:54 -0500

    usando destructores

--- 0x04-class/main.cpp
+++ 0x04-class/main.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 class Persona {
     public:
-        string nombre = "Juan";
-        int edad = 24;
+        string nombre;
+        int edad;
+
+        Persona(string n, int e) {
+            nombre = n;
+            edad = e;
+        }
+        ~Persona() {
+            cout << "destructor" << endl;
+        }
         void saludo(string n) {
             cout << "Hola " << n << endl;
         }
 };
 
 int main() {
-    Persona p = Persona();
-    Persona p2 = Persona();
+    Persona p = Persona("Juan", 24);
+    Persona p2 = Persona("Pedro", 22);
 
-    p2.nombre = "Pedro";
+    // delete p2; solo con punteros.
 
     cout << p.nombre << endl;
     p.saludo(p.nombre);
